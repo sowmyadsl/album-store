0adf209cc644c4beec6e2534a2e906121059809a
sowmya dinavahi <sowmya.dsl@gmail.com>
2017-06-14 00:08:34 -0700

    Create a album model to add albums to marketplace

---
+++ src/app/album.model.ts
@@ -0,0 +1,9 @@
+// model with properties
+
+export class Album {
+  constructor (
+    public title: string,
+    public artist: string,
+    public description: string,
+    public id: number){}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { routing } from './app.routing';
+import { AboutComponent } from './about/about.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    AboutComponent,
+    MarketplaceComponent,
+    AlbumDetailComponent
   ],
   imports: [
     BrowserModule,
